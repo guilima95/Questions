5f4a10e7de99d3bf2925e7b161b6ae799fab2293
Warley Silva Lima <warley.lima@stoque.com.br>
2019-09-30 15:10:29 -0300

    Criando método para recuperar valores do Json.

--- src/app/app-quiz.service.ts
+++ src/app/app-quiz.service.ts
@@ -1,19 +1,27 @@
+import { Component } from '@angular/core';
 import {QuizModel} from '../app/models/quiz.model';
+import dataQuiz from './data/db.json';
+
+@Component({
+    selector: 'app-root'
+})
 
 export class QuizService{
-    private fs = require('fs');
+    //private fs = require('fs');
     private quizArray: Array<QuizModel>;
 
     public async GetQuizzes(): Promise<Array<QuizModel>>{
-        this.fs.readFile('../app/data/db.json', handleJSONFile);
+        //this.fs.readFile('../app/data/db.json', handleJSONFile);
 
-        var handleJSONFile = async function(err, data){
-            if(err){
-                throw err;
-            }
+        // var handleJSONFile = async function(err, data){
+        //     if(err){
+        //         throw err;
+        //     }
 
-            this.quizArray = await JSON.parse(data);
-        }
+        //     this.quizArray = await JSON.parse(data);
+        // }
+
+        this.quizArray = dataQuiz;
 
         return this.quizArray;
     }
